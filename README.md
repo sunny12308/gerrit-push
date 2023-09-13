8bb487e618b65f409fc4472d552d18cdd4ecc103
孙瑜00295490 <sun.yu5@zte.com.cn>
2023-09-13 11:14:25 +0800

    记住评审人

--- README.md
+++ README.md
@@ -1,9 +1,7 @@
-[![](https://vsmarketplacebadge.apphb.com/version-short/aspel.gerrit-push.svg)](https://marketplace.visualstudio.com/items?itemName=aspel.gerrit-push)
-[![](https://vsmarketplacebadge.apphb.com/downloads-short/aspel.gerrit-push.svg)](https://marketplace.visualstudio.com/items?itemName=aspel.gerrit-push)
-[![](https://vsmarketplacebadge.apphb.com/rating-short/aspel.gerrit-push.svg)](https://marketplace.visualstudio.com/items?itemName=aspel.gerrit-push)
 # Gerrit Push README
 
 This plugin allows you to push commits into a Gerrit (HEAD:refs/for/< branch >)
 
 ⌘ ⇧ P (Cmd + Shift + P) => Gerrit Push
-# 记住评审人
+# 输入评审人
+可以记住评审人
--- src/extension.ts
+++ src/extension.ts
@@ -1,44 +1,46 @@
 import * as vscode from 'vscode';
 import * as path from 'path';
 
-async function mainGerrit() {
-  const repoRaw = await gitAPI("repos")
+async function mainGerrit(context: vscode.ExtensionContext) {
+  const repoRaw = await gitAPI("repos");
   const repos: any = [];
   repoRaw.forEach((value: any, index: number) => {
-    const r_name = path.basename(value.repository.root);
-    const r_desc = [value.repository.headLabel, value.repository.syncLabel]
+    const rName = path.basename(value.repository.root);
+    const rDesc = [value.repository.headLabel, value.repository.syncLabel]
     .filter(l => !!l)
     .join(' ');
-    repos.push({ id: index, label: r_name, description: r_desc })
+    repos.push({ id: index, label: rName, description: rDesc });
   });
-  const repo_id: any = await showRepoQuickPick(repos)
+  const repoId: any = await showRepoQuickPick(repos);
 
-  const branchRaw = await gitAPI("branch", "", repo_id['id'])
-  const branch: string[] = [];
+  const branchRaw = await gitAPI("branch", "", repoId['id']);
+  const branchs: string[] = [];
   branchRaw.forEach(function (value: any) {
-    branch.push(value['name'])
+    branchs.push(value['name']);
   });
-  showBranchQuickPick(branch, repo_id['id'])
+  const branch:any=await showBranchQuickPick(branchs);
+ const reviewers:any= await showReviewersInput(context);
+  gitAPI("push", branch.label+reviewers,  repoId['id'],);
 }
 
-async function gitAPI(val: string, push_branch: string = "", id: number = 0) {
-  const gitExtension = vscode.extensions.getExtension('vscode.git')
-  const git = gitExtension?.exports
+async function gitAPI(val: string, pushBranch: string = "", id: number = 0) {
+  const gitExtension = vscode.extensions.getExtension('vscode.git');
+  const git = gitExtension?.exports;
   const api = git.getAPI(1);
-  if (val == "branch") {
+  if (val === "branch") {
     const repo = api.repositories[id];
     const branch = await repo.getBranches("origin");
-    return branch
-  } else if (val == "push") {
+    return branch;
+  } else if (val === "push") {
     const repo = api.repositories[id];
-    repo.push("origin", `HEAD:refs/for/${push_branch}`)
+    repo.push("origin", `HEAD:refs/for/${pushBranch}`)
       .catch((err: any) => {
-        vscode.window.showErrorMessage(err.stderr)
-      })
-  } else if (val == "repos") {
+        vscode.window.showErrorMessage(err.stderr);
+      });
+  } else if (val === "repos") {
     const repo = api.repositories;
-    console.log(repo)
-    return repo
+    console.log(repo);
+    return repo;
   }
 }
 
@@ -46,35 +48,34 @@ async function showRepoQuickPick(val: any) {
   const result = await vscode.window.showQuickPick(val, {
     placeHolder: 'Select your workdir',
   });
-  return result
+  return result;
 }
 
-function showBranchQuickPick(codes: any, id: any) {
-  return new Promise((resolve) => {
-    const quickPick = vscode.window.createQuickPick()
-    quickPick.placeholder = 'Select (or create) HEAD:refs/for/<branch>'
-    quickPick.canSelectMany = false
-    quickPick.items = codes.map((label: any) => ({ label }))
-    quickPick.onDidAccept(async () => {
-      const selection = quickPick.activeItems[0]
-      resolve(selection.label)
-      gitAPI("push", selection.label, id)
-      quickPick.hide()
-    })
-    quickPick.onDidChangeValue(() => {
-      // add a new code to the pick list as the first item
-      if (!codes.includes(quickPick.value)) {
-        const newItems = [quickPick.value, ...codes].map(label => ({ label }))
-        quickPick.items = newItems
-      }
-    })
-    quickPick.onDidHide(() => quickPick.dispose())
-    quickPick.show()
-  })
+function showBranchQuickPick(codes: any) {
+  return vscode.window.showQuickPick(codes.map((label: any) => ({ label })),{
+    placeHolder:'Select (or create) HEAD:refs/for/<branch>',
+    canPickMany:false
+  });
+}
+async function showReviewersInput(context: vscode.ExtensionContext) {
+  // 从 workspaceState 中读取保存的数据
+  const storedValue:string = context.workspaceState.get('gerritReviewers') as string;
+  let dReviewers:string=storedValue||'';
+
+  let value=await  vscode.window.showInputBox({
+    placeHolder:'input reviewers(%r=xx,r=xx)',
+    value:dReviewers
+  });
+
+  //更新
+  context.workspaceState.update('gerritReviewers', value);
+  return value;
 }
 
 export function activate(context: vscode.ExtensionContext) {
-  let disposable = vscode.commands.registerCommand('gerrit.push', mainGerrit);
+  let disposable = vscode.commands.registerCommand('gerrit.push', function(){
+    mainGerrit(context);
+  });
   context.subscriptions.push(disposable);
 }
 export function deactivate() { }
